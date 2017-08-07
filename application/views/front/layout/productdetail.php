21a0cf9493998f510b67caada70774c6c4c0f604
Tuấn anh <phamtuananh@gmail.com>
2017-08-07 23:46:08 +0700

    update

--- application/views/front/layout/productdetail.php
+++ application/views/front/layout/productdetail.php
@@ -3,13 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="single-sidebar">
-                        <h2 class="sidebar-title">Tìm kiếm sản phẩm</h2>
-                        <form action="">
-                            <input type="text" placeholder="Search products...">
-                            <input type="submit" value="Search">
-                        </form>
-                    </div>
+                    <?php  $this->load->view('front/layout/search')?>
                     
                     <div class="single-sidebar">
                         <h2 class="sidebar-title">Sản phẩm liên quan</h2>
