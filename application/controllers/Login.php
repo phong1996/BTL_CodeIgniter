be02b4ec9458fe03b503aee7dfab8e2dcba68d36
tientom2017 <leyentien96@gmail.com>
2017-08-07 16:23:06 +0700

    up

---
+++ application/controllers/Login.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends MY_Controller {
+	 function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Login_model');
+
+    }
+	public function index()
+	{
+		$this->load->view('front/login/login');
+	}
+	 public function login()
+    {
+        $username = $this->input->post('username');
+        $passwords = $this->input->post('password');
+
+        $password = md5($passwords);
+
+        if($this->Login_model->checkLoginCustomer($username,$password))
+        {
+
+           /* if($this->input->post('remember') != '')
+            {
+                $this->load->helper('cookie');
+                $cookie['user'] = array(
+                    'name'   => 'user',
+                    'value'  => $username,
+                    'expire' => 60 * 60 * 24,
+                    'domain' => 'localhost',
+                    'path'   => '/',
+                );
+                $cookie['pass'] = array(
+                    'name'   => 'pass',
+                    'value'  => $passwords,
+                    'expire' => 60 * 60 * 24,
+                    'domain' => 'localhost',
+                    'path'   => '/',
+                );
+                $this->input->set_cookie($cookie['user']);
+                $this->input->set_cookie($cookie['pass']);
+            }*/
+
+
+            $where = array('username' => $username, 'password' => $password);
+            $user = $this->Login_model->get_info_rule($where);
+            $this->session->set_userdata('user_login', $user);
+            $this->session->set_flashdata('flash_message', 'Đăng nhập thành công');
+            return redirect('home');
+        }
+        else
+        {
+        	$this->session->set_flashdata('fail_login', 'Đăng nhập Thất bại');
+            return redirect('login');
+        }
+       
+    }
+
+    public function logOut()
+    {
+        $this->session->unset_userdata("user_login");
+        redirect('login');
+    }
+}
---
+++ application/views/front/login/login.php
@@ -0,0 +1,69 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Khách Hàng Đăng Nhập</title>
+<script src="<?php echo public_url() ?>/login/js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="<?php echo public_url() ?>/login/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Classy Login form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+</head>
+<body>
+<!--header start here-->
+<div class="header">
+		<div class="header-main">
+		       <h1>Khách Hàng Đăng Nhập</h1>
+			<div class="header-bottom">
+				<div style="color: red;text-align: center;">
+					<?php echo $this->session->flashdata('fail_login'); ?>
+				</div>
+				<div class="header-right w3agile">
+					<div class="header-left-bottom agileinfo">
+					 <form action="login/login" method="post">
+						<input type="text" name="username" placeholder="Tài khoản" />
+						<input type="password" name="password" placeholder="Mật khẩu" />
+						<div class="remember">
+			             <span class="checkbox1">
+							   <label class="checkbox"><input type="checkbox" name="remember" ><i> </i>Duy trì đăng nhập</label>
+						 </span>
+						 <div class="forgot">
+						 	<h6><a href="#">Quên mật khẩu?</a></h6>
+						 </div>
+						<div class="clear"> </div>
+					  </div>
+					   
+						<input name="login" type="submit" value="Đăng Nhập">
+					</form>	
+					<div class="header-left-top">
+						<div class="sign-up"> <h2>Hoặc</h2> </div>
+					
+					</div>
+					<div class="header-social wthree">
+							<a href="#" class="face"><h5>Facebook</h5></a>
+							<a href="#" class="twitt"><h5>Twitter</h5></a>
+						</div>
+						
+				</div>
+				</div>
+			  
+			</div>
+		</div>
+</div>
+<!--header end here-->
+<div class="copyright">
+	<p>© 2017 Ustora mobile | Design by Team 14 </a></p>
+</div>
+<!--footer end here-->
+</body>
+</html>
