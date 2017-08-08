b8844ed912384e7c51ec54f87069c46fe73fed30
tientom2017 <leyentien96@gmail.com>
2017-08-08 21:00:42 +0700

    ae oi

--- application/views/front/layout/master.php
+++ application/views/front/layout/master.php
@@ -58,7 +58,7 @@
                        <?php if(!isset($this->session->userdata('customer_login')->fullname)){
                            ?>
                            <li><a class="cd-signin" href="<?php echo base_url()?>/login"><i class="fa fa-user"></i> Đăng nhập</a></li>
-                           <li><a href="#"><i class="fa fa-users" aria-hidden="false"></i> Đăng ký</a></li>
+                           <li><a href="<?php echo base_url()?>register"><i class="fa fa-users" aria-hidden="false"></i> Đăng ký</a></li>
                            <?php
                        } ?>
                         <?php if(isset($this->session->userdata('customer_login')->fullname)){
